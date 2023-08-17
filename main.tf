f79de3675e2bc322855360cb1f04e156a44eb971
sumit <sumitbhatia101@gmail.com>
2023-08-17 22:26:27 +0530

    Okss

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "ap-southeast-2"
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" #
+  ami           = "ami-0310483fb2b488153" #
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-Instance"
+    Name = "TF-Instance"
   }
 }
